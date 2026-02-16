4cb9c266762fbabba233591ffdcb8f24627333fe
Gourav Dutta <duttagourav047@gmail.com>
2026-02-16 12:25:31 +0530

    Better-Auth 3rd commit

---
+++ src/app/actions/auth.ts
@@ -0,0 +1,82 @@
+// "use server";
+
+import { userRole } from "@/generated/prisma/enums";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { NextResponse } from "next/server";
+
+export async function signUpAction(formData: FormData) {
+  try {
+    const name = formData.get("name") as string;
+    const password = formData.get("password") as string;
+    const email = formData.get("email") as string;
+    const phone_no = formData.get("phone_no") as string;
+    const year = formData.get("year") as string;
+    const role = formData.get("role") as userRole;
+
+    const newUser = await auth.api.signUpEmail({
+      body: {
+        name,
+        email,
+        password,
+        phone_no,
+        role,
+        year,
+      },
+    });
+    return NextResponse.json(
+      {
+        msg: "User creted",
+        data: newUser,
+      },
+      { status: 201 },
+    );
+  } catch (err: any) {
+    return NextResponse.json(
+      {
+        msg: `An error occured: ${err.message}`,
+      },
+      { status: 500 },
+    );
+  }
+}
+
+export async function SignInAction(formData: FormData) {
+  try {
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+
+    const user = await auth.api.signInEmail({
+      body: {
+        email,
+        password,
+      },
+    });
+
+    return NextResponse.json({
+      msg: "User logged in Successfully",
+      data: user,
+    });
+  } catch (err: any) {
+    return NextResponse.json(
+      {
+        msg: `An error occured: ${err.message}`,
+      },
+      { status: 500 },
+    );
+  }
+}
+
+export async function SignOutAction() {
+  try {
+    await auth.api.signOut({
+      headers: await headers(),
+    });
+    return NextResponse.json({
+      msg: "Logged Out"    })
+  } catch (err: any) {
+    return NextResponse.json({
+      msg: `An error occured: ${err.message}`
+    })
+  }
+}
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,7 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { PrismaClient } from "@/generated/prisma/client";
+import { nextCookies } from "better-auth/next-js";
 
 const prisma = new PrismaClient();
 export const auth = betterAuth({
@@ -10,4 +11,12 @@ export const auth = betterAuth({
     emailAndPassword: { 
     enabled: true, 
   }, 
+  user: {
+        additionalFields: {
+            role: { type: "string", required: true },
+            year: { type: "number", required: true },
+            phone_no: { type: "string", required: true }
+        }
+    },
+  plugins: [nextCookies()]
 });
